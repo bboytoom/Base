48a59271aecfbcd1b9d1714c6b2f9c8aee8ddb77
bboytoom <thomasrosber@gmail.com>
2019-04-14 00:44:41 -0500

    Instalacion de las herramientas para el desarrollo del front-end

--- Administrator/App_Start/RouteConfig.cs
+++ Administrator/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace Administrator
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}",
+                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+'use strict';
+
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let autoprefixer = require('gulp-autoprefixer');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglify');
+let cssnano = require('gulp-cssnano');
+let imagemin = require('gulp-imagemin');
+let ts = require("gulp-typescript");
+let tsProject = ts.createProject("tsconfig.json");
+
+let GlobalSass = './src/sass/main.scss';
+let ManagerSass = './src/sass/custom-manager.scss';
+let BaseImage = './src/images/';
+
+let Jquery = './node_modules/jquery/dist/jquery.min.js';
+let Popper = './node_modules/popper.js/dist/umd/popper.min.js';
+let BootstrapJs = './node_modules/bootstrap/dist/js/bootstrap.min.js';
+let MaterialJs = './node_modules/material-design-lite/material.min.js';
+
+let Destino = './Administrator/Assets/';
+
+gulp.task('global-css', function () {
+    return gulp.src(GlobalSass)
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(concat('build.min.css'))
+        .pipe(gulp.dest(Destino + 'css'));
+});
+
+gulp.task('manager-css', function () {
+    return gulp.src(ManagerSass)
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(concat('build-manager.min.css'))
+        .pipe(gulp.dest(Destino + 'css'));
+});
+
+gulp.task('global-image', function () {
+    return gulp.src(BaseImage + '/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest(Destino + 'images'))
+});
+
+gulp.task('global-js', function () {
+    return gulp.src([Jquery, Popper])
+        .pipe(concat('build.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(Destino + 'js'));
+});
+
+gulp.task('globalLibs-js', function () {
+    return gulp.src([BootstrapJs, MaterialJs])
+        .pipe(concat('build-libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(Destino + 'js'));
+});
+
+gulp.task('custom-js', function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest(Destino + 'js'));
+});
+
+gulp.watch(Destino + 'css/*.css', gulp.series('global-css', 'manager-css'));
+gulp.watch(Destino + 'js/*.js', gulp.series('global-js', 'globalLibs-js', 'custom-js'));
+
+gulp.task('default',
+    gulp.parallel(
+        'global-css',
+        'manager-css',
+        'global-image',
+        'global-js',
+        'globalLibs-js',
+        'custom-js'
+    )
+);
